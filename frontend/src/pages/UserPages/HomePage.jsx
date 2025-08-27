14a4bfac13bc4b9cbb887018c508e28af4a1f1c6
Antuchakma <biposhyan@gmail.com>
2025-08-27 14:48:01 +0600

    installed tailwind

--- frontend/src/pages/UserPages/HomePage.jsx
+++ frontend/src/pages/UserPages/HomePage.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const s = () => {
+const HomePage = () => {
   return (
     <div>
-      hello world
+      
     </div>
   )
 }
 
-export default s
+export default HomePage
