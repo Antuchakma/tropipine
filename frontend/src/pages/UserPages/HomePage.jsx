93af94b1bad98ead6e94c972297837bf76a24ca6
Antuchakma <biposhyan@gmail.com>
2025-08-28 02:39:29 +0600

    continuation

--- frontend/src/pages/UserPages/HomePage.jsx
+++ frontend/src/pages/UserPages/HomePage.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div> 
-      
+    <div>
+      {/*navbar*/} 
+      <nav className='flex justify-between p-4'>
+
+      <div className="flex-1">
+    <a className="btn btn-ghost normal-case text-xl">MyApp</a>
+  </div>
+      <div> hello</div>
+      </nav>
+
+      {/* hero section*/}
+      <div>
+
+      </div>
+
     </div>
   )
 }
